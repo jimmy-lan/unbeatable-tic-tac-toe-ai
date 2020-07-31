4746914ecbaca169be8153b09498c14aa806f49a
Jimmy <jimmylanbcn@gmail.com>
2020-07-30 20:54:38 -0400

    Add isEmpty and isFull helper

--- src/classes/Board.js
+++ src/classes/Board.js
@@ -2,4 +2,18 @@ export class Board {
   constructor(state = new Array(9).fill("")) {
     this.state = state;
   }
+
+  /**
+   * Return whether the board is empty
+   */
+  isEmpty() {
+    return this.state.every((mark) => !mark);
+  }
+
+  /**
+   * Return whether the board is full
+   */
+  isFull() {
+    return this.state.every((mark) => mark);
+  }
 }
