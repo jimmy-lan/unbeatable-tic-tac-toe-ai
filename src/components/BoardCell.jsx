7fea9bae574c0ff592bc22a73fbc085182aecb47
Jimmy <jimmylanbcn@gmail.com>
2020-07-31 17:39:52 -0400

    Update styles

--- src/components/BoardCell.jsx
+++ src/components/BoardCell.jsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, Grid, makeStyles } from "@material-ui/core";
+import { Close as CloseIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    width: "100%",
+    height: "100%",
+    minHeight: 150,
+    borderRadius: 0,
+    backgroundColor: "#fff",
+  },
+  circle: {
+    width: 42,
+    height: 42,
+    border: "solid",
+    borderWidth: 6,
+    borderColor: "#333",
+    borderRadius: "50%",
+    display: "inline-block",
+  },
+  close: {
+    width: 72,
+    height: 72,
+  },
+}));
 
 export const BoardCell = (props) => {
+  const classes = useStyles();
+
+  const { mark } = props;
+
+  const renderMark = () => {
+    if (mark === "circle") {
+      return <span className={classes.circle} />;
+    } else if (mark === "cross") {
+      return <CloseIcon className={classes.close} />;
+    }
+    return <></>;
+  };
+
   return (
-    <Button variant="contained" disableElevation>
-      Board Cell
-    </Button>
+    <Grid item xs={4} {...props}>
+      <Button className={classes.button} disableElevation>
+        {renderMark()}
+      </Button>
+    </Grid>
   );
 };
---
+++ src/containers/Board.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BoardCell } from "../components/BoardCell";
+import { Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(2),
+    background: "#f0f0f0",
+    maxWidth: 450 + theme.spacing(2),
+  },
+  boardRow: {
+    margin: "0 4px",
+    padding: 2,
+  },
+}));
+
+const BoardRow = (props) => {
+  return (
+    <Grid container item direction="row" xs={12} spacing={1} {...props}>
+      <BoardCell />
+      <BoardCell />
+      <BoardCell />
+    </Grid>
+  );
+};
+
+export const Board = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <BoardRow className={classes.boardRow} />
+        <BoardRow className={classes.boardRow} />
+        <BoardRow className={classes.boardRow} />
+      </Grid>
+    </div>
+  );
+};
